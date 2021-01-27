bec5e61ef9e3f39f9b01f20fb0fa2d2e60843685
Santiago Laiton <santiago.laiton@mail.escuelaing.edu.co>
2021-01-27 09:50:09 -0500

    punto 1

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,12 +1,37 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
+	private static AtomicInteger cont =new AtomicInteger(0);
+
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		PrimeFinderThread pft1=new PrimeFinderThread(0, 9999999,cont);
+		PrimeFinderThread pft2=new PrimeFinderThread(10000000, 19999999,cont);
+		PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000,cont);
+
 		
-		pft.start();
+		pft1.start();
+		pft2.start();
+		pft3.start();
+
+		long start = System.currentTimeMillis();
+		long end = start + 5*1000;
+		System.out.println();
+		while (System.currentTimeMillis() < end) {
+			//System.out.println((end-start)/1000);
+		}
+		//System.out.println(System.currentTimeMillis()/1000+"Tiempo");
+
+		pft1.stop();
+		pft2.stop();
+		pft3.stop();
+
+		System.out.println(cont.get()+"Numero de primos encontrados");
+
+
 		
 		
 	}
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -2,27 +2,34 @@ package edu.eci.arsw.primefinder;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrimeFinderThread extends Thread{
 
 	
 	int a,b;
+	AtomicInteger cont;
 	
 	private List<Integer> primes=new LinkedList<Integer>();
 	
-	public PrimeFinderThread(int a, int b) {
+	public PrimeFinderThread(int a, int b,AtomicInteger cont) {
 		super();
 		this.a = a;
 		this.b = b;
+		this.cont=cont;
 	}
 
 	public void run(){
 		for (int i=a;i<=b;i++){						
 			if (isPrime(i)){
 				primes.add(i);
-				System.out.println(i);
+				cont.getAndIncrement();
+				//System.out.println(i);
 			}
 		}
+
+
+
 		
 		
 	}
